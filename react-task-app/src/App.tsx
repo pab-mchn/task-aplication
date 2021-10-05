d7657542cab1db3ae2dcfb8635a6b237a03c9880
pablo michelini <pab.michelini@gmail.com>
2021-10-06 00:11:21 +0200

    add styles in form

--- react-task-app/src/App.tsx
+++ react-task-app/src/App.tsx
@@ -26,17 +26,26 @@ function App(): JSX.Element {
   }
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <input type="text" onChange={e => setnewTask(e.target.value)} value={newTask}/>
-        <button>Save</button>
-      </form>
-      {
-        tasks.map((t: ITask, i: number) => {
-          return <h1 key={i}>{t.name}</h1>
-        })
-      }
-    </>
+    <div className="container p-4">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <div className="card">
+            <div className="card-body">
+              <form onSubmit={handleSubmit}>
+                <input type="text" onChange={e => setnewTask(e.target.value)} value={newTask} className="form-control" />
+                <button className="btn btn-danger btn-block mt-2">Save</button>
+              </form>
+            </div>
+          </div>
+          {
+            tasks.map((t: ITask, i: number) => {
+              return <h1 key={i}>{t.name}</h1>
+            })
+          }
+        </div>
+      </div>
+
+    </div>
   );
 }
 
