cb186f7b3b08ca35d94b7a98037421c90cb346bc
pablo michelini <pab.michelini@gmail.com>
2021-10-05 23:30:07 +0200

    add new task in the list and clean input after done

--- react-task-app/src/App.tsx
+++ react-task-app/src/App.tsx
@@ -1,20 +1,34 @@
 import React, {useState} from 'react';
 
 type FormElement = React.FormEvent<HTMLFormElement>;
+interface ITask {
+  name: string;
+  done: boolean;
+}
 
 function App(): JSX.Element {
+  //create new task
+  const [newTask, setnewTask] = useState<string>('')
+  //add a new task in the list
+  const [tasks, setTasks] = useState<ITask[]>([])
 
-  const [newTask, setnewTask] = useState('')
-
+  //function for create new task
   const handleSubmit = (e: FormElement) => {
     e.preventDefault();
-    console.log(newTask)
+    addTask(newTask)
+    console.log(tasks);
+  }
+
+  //function for add a new task in the list
+  const addTask = (name: string) => {
+    const newTasks: ITask[] = [...tasks, {name : name, done: false}]
+    setTasks(newTasks)
   }
 
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input type="text" onChange={e => setnewTask(e.target.value)} />
+        <input type="text" onChange={e => setnewTask(e.target.value)} value={newTask}/>
         <button>Save</button>
       </form>
     </>
